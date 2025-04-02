93864855be8553fb5bd4696bbae8b8906626b99d
LU Yan <yludk@connect.ust.hk>
2025-04-02 18:13:29 +0800

    Update IRSensing.cpp

--- IRSensing.cpp
+++ IRSensing.cpp
@@ -12,7 +12,7 @@ void IR::Init(){
   1: Black*/
 
 bool IR::OnStartingLine(){
-  if(digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R))){
+  if(digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R))){ //111 start
     return true; 
   }
   else{
@@ -23,22 +23,22 @@ bool IR::OnStartingLine(){
 
 uint8_t IR::Tracking(){
   //M_IR on Track
-  if(digitalRead(IR_M) && !(digitalRead(IR_L)) && !(digitalRead(IR_R)))
+  if(digitalRead(IR_M) && !(digitalRead(IR_L)) && !(digitalRead(IR_R))) //! mean '0',white, 010
     return OnTrack;
   //R_IR on Track 
-  if((!digitalRead(IR_M) && (!digitalRead(IR_L)) && (digitalRead(IR_R))) || (digitalRead(IR_M) && (!digitalRead(IR_L)) && (digitalRead(IR_R))))
+  if((!digitalRead(IR_M) && (!digitalRead(IR_L)) && (digitalRead(IR_R))) || (digitalRead(IR_M) && (!digitalRead(IR_L)) && (digitalRead(IR_R)))) //001 or 011
     return IR_ROnTrack;  
   //L_IR on Track 
-  if(!digitalRead(IR_M) && (digitalRead(IR_L)) && !(digitalRead(IR_R)) || (digitalRead(IR_M) && (digitalRead(IR_L)) && !(digitalRead(IR_R))))
+  if(!digitalRead(IR_M) && (digitalRead(IR_L)) && !(digitalRead(IR_R)) || (digitalRead(IR_M) && (digitalRead(IR_L)) && !(digitalRead(IR_R)))) //100 or 110
     return IR_LOnTrack;
   //L_IR and R_IR are on Track
-  if(!digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R)))// || (digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R))))
+  if(!digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R)))// || (digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R)))) //101
     return IR_LandROnTrack;
   //All on Track
-  if(digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R)))
+  if(digitalRead(IR_M) && (digitalRead(IR_L)) && (digitalRead(IR_R))) //111
     return AllOnTrack;
   
 
-  return OutOfTrack; 
+  return OutOfTrack; //000
 };
 
