298cdbae429434f52320fe40fd04b12906fcb2d4
Fiona FENG Shiqi <sfengam@connect.ust.hk>
2025-04-30 15:40:17 +0800

    Add files via upload
    0430_v1
    1. credentials.hpp all set up
    2. codes for ChassisSkeletonCode.ino

---
+++ IoTCar/credential.hpp
@@ -0,0 +1,13 @@
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "ISD-Project-22Fall" // Network SSID (name)
+#define WIFI_PASSWORD "isd@2022Fall" // Network password
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyA-SRo0L3bn18FM9BzcX9Zsb5W6xBFdIt0"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "https://isdn2602-inx-default-rtdb.asia-southeast1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "fionafeng721@gmail.com"
+#define USER_PASSWORD "12345678"
